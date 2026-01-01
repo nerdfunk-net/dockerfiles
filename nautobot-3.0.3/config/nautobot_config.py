23b1447319a6f4a9081430b9ef54e54c7987bb63
Marc <86735509+nerdfunk-net@users.noreply.github.com>
2026-01-01 15:17:41 +0100

    nautobot 3.0.3; keycloak;tig

---
+++ nautobot-3.0.3/config/nautobot_config.py
@@ -0,0 +1,58 @@
+"""Nautobot development configuration file."""
+
+# pylint: disable=invalid-envvar-default
+import os
+import sys
+
+from nautobot.core.settings import *  # noqa: F403  # pylint: disable=wildcard-import,unused-wildcard-import
+from nautobot.core.settings_funcs import is_truthy, parse_redis_connection
+
+#
+# Debug
+#
+
+DEBUG = is_truthy(os.getenv("NAUTOBOT_DEBUG", False))
+
+TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
+
+#
+# Logging
+#
+
+LOG_LEVEL = "DEBUG" if DEBUG else "INFO"
+
+#
+# Redis
+#
+
+# Redis Cacheops
+CACHEOPS_REDIS = parse_redis_connection(redis_database=1)
+
+#
+# Celery settings are not defined here because they can be overloaded with
+# environment variables. By default they use `CACHES["default"]["LOCATION"]`.
+#
+
+# Enable installed plugins. Add the name of each plugin to the list.
+PLUGINS = ["nautobot_ssot", "nautobot_plugin_nornir", "nautobot_device_onboarding"]
+PLUGINS_CONFIG = {
+    "nautobot_ssot": {
+        "hide_example_jobs": True
+    },
+    "nautobot_plugin_nornir": {
+        "nornir_settings": {
+            "credentials": "nautobot_plugin_nornir.plugins.credentials.nautobot_secrets.CredentialsNautobotSecrets",
+            "runner": {
+                "plugin": "threaded",
+                "options": {
+                    "num_workers": 20,
+                },
+            },
+        },
+    },
+}
+# Plugins configuration settings. These settings are used by various plugins that the user may have installed.
+# Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
+PLUGINS_CONFIG = {
+    "nautobot_example_plugin": {},
+}
